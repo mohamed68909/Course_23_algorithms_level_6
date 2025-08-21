8adcbf741c35dfd9e725f7ea4c06b6e26cbe318e
mohamed68909 <m6377116@gmail.com>
2025-08-21 11:34:03 +0300

    Add solution Problem 2

---
+++ Problem 2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Problem_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problem : Convert a decimal number to binary using a stack.
+            Console.Write("Enter a decimal number: ");
+            int decimalNumber = int.Parse(Console.ReadLine());
+            Stack<int> binaryStack = new Stack<int>();
+            while (decimalNumber > 0)
+            {
+                binaryStack.Push(decimalNumber % 2);
+                decimalNumber /= 2;
+            }
+            Console.Write("Binary representation: ");
+            while (binaryStack.Count > 0)
+            {
+                Console.Write(binaryStack.Pop());
+            }
+            
+
+        }
+    }
+}
